26b259e199431dfe4de35d70ffd2306892f8fcb0
Will Speak <lithiumflame@gmail.com>
2014-05-22 21:53:16 +0100

    Add A Class
     * prints out the parameters to the main method
     * uses another class
     * FEATURES!

--- main.cs
+++ main.cs
@@ -1,10 +1,24 @@
 using System;
 
+public class Application
+{
+	public void Run(string[] argv)
+	{
+		String world = "world";
+		Console.WriteLine("Hello {0}", world);
+
+		foreach (string arg in argv)
+		{
+			Console.WriteLine(arg);
+		}
+	}
+}
+
 public class Foo
 {
-  public static void Main()
+  public static void Main(string[] argv)
   {
-	String world = "world";
-	Console.WriteLine("Hello {0}", world);
+	Application app = new Application();
+	app.Run(argv);
   }
 }
